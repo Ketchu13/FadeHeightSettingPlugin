062f6adc494e4b1276b09464bbbda2572f0a36e3
Nicolas Grimaud <ketchu13@hotmail.com>
2021-03-12 17:33:45 +0100

    Update script to cura 4.8

--- __init__.py
+++ __init__.py
@@ -2,8 +2,6 @@
 # The FadeHeightSettingPlugin is released under the terms of the AGPLv3 or higher.
 
 from . import FadeHeightSettingPlugin
-from UM.i18n import i18nCatalog
-i18n_catalog = i18nCatalog("FadeHeightSettingPlugin")
 
 
 def getMetaData():
